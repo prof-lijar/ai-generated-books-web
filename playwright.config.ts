3fcdaa846620b7edf04d14f6a242b902681c2fed
prof-lijar <davidbeljar@gmail.com>
2026-05-28 15:36:58 +0900

    [QA] Fix corrupted package.json and update playwright.config.ts with webServer

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,6 +11,13 @@ export default defineConfig({
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
   },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+    stdout: 'pipe',
+    stderr: 'pipe',
+  },
   projects: [
     {
       name: 'chromium',
